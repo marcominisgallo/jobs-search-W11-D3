38e1f1d9d0ebe294c6ca261a691a36de3b43b702
Marco <marcominisgallo@yahoo.it>
2025-04-16 17:28:34 +0200

    .

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,10 +1,10 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Job = ({ data }) => (
   <Row
     className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
+    style={{ border: "1px solid #00000033", borderRadius: 4 }}
   >
     <Col xs={3}>
       <Link to={`/${data.company_name}`}>{data.company_name}</Link>
@@ -15,6 +15,6 @@ const Job = ({ data }) => (
       </a>
     </Col>
   </Row>
-)
+);
 
-export default Job
+export default Job;
